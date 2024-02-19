0a68725c317b2dc47763550927d2059aa41645e2
Filip Pustovoitenko <pustovoitenkofilip@gmail.com>
2024-02-19 21:17:55 +0200

    feat: add jwt + other auth stuff update old files

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -8,9 +8,10 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="1yNz57rQiT2vfaPaBYjtWy3Z6K2Jo6y3E
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
-EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
-EMAIL_HOST = env("EMAIL_HOST", default='mailhog')
-EMAIL_HOST = env("EMAIL_PORT")
-DEFAULT_FROM_EMAIL = 'support@apiimperfect.site'
-DOMAIN = env('DOMAIN')
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
 SITE_NAME = "Authors Haven"
--- core_apps/users/forms.py
+++ core_apps/users/forms.py
@@ -13,15 +13,16 @@ class UserChangeForm(admin_forms.UserChangeForm):
 class UserCreationForm(admin_forms.UserCreationForm):
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
-        fields = ['first_name', 'last_name', 'email']
-        error_messages = {
-            'duplicate_email': "A user with this email already exists"
-        }
+        fields = ("first_name", "last_name", "email")
 
-        def clean_email(self):
-            email = self.cleaned_data['email']
-            try:
-                User.objects.get(email=email)
-            except:
-                return email
-            raise forms.ValidationError(self.error_messages['duplicate_email'])
+    error_messages = {
+        "duplicate_email": "A user with this email already exists.",
+    }
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError(self.error_messages["duplicate_email"])
